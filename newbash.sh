22ad4d126039347fa220da14bd54ae7ea41495e8
Razorops-code <110587174+Razorops-code@users.noreply.github.com>
2023-01-25 17:16:33 +0530

    Update newbash.sh

--- newbash.sh
+++ newbash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
+curl --request GET \
+  --url 'https://codenginx.atlassian.net//rest/api/3/events' \
+  --user 'rajashekarkoppula5@gmail.com:3xgjCoflshD6yfNPYlm6A01E' \
+  --header 'Accept: application/json'
 
 # Set the endpoint URL
 URL="https://codenginx.atlassian.net/rest/api/2/issue"
-TOKEN=vwInbPFWnMtkq8ASNuGz92FE
+TOKEN=3xgjCoflshD6yfNPYlm6A01E
 EMAIL=rajashekarkoppula5@gmail.com
 
 # Set the request data
